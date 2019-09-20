27daf363c8e8aa8800f88ee55c71518e5ca38dfa
Mauricio Giacomini Girardello <mauriciogiacomini4@gmail.com>
2019-09-20 10:05:01 -0300

    Adding main supervisor

--- lib/message_broker.ex
+++ lib/message_broker.ex
@@ -1,13 +1,52 @@
 defmodule MessageBroker do
   @moduledoc """
-  Documentation for MessageBroker.
+  MessageBroker aims to publish events
   """
 
+  use Supervisor
+
+  alias MessageBroker.Publisher
+  alias MessageBroker.Notifier
+
+  def start_link(opts \\ []) do
+    Supervisor.start_link(__MODULE__, opts, name: __MODULE__)
+  end
+
+  @impl true
+  def init(_opts) do
+    children = [
+      Publisher,
+      Notifier
+    ]
+
+    Supervisor.init(children, strategy: :one_for_one)
+  end
+
+  @spec config :: MessageBroker.Config.t()
   def config, do: struct(MessageBroker.Config, Application.get_all_env(:message_broker))
 
-  defmodule MessageBroker.Config do
+  @spec get_config(atom()) :: any
+  def get_config(key), do: Map.get(config(), key)
+
+  defmodule Config do
     @moduledoc false
 
-    defstruct [:repo]
+    @type t :: %__MODULE__{
+            repo: Ecto.Repo.t(),
+            rabbitmq_user: String.t(),
+            rabbitmq_password: String.t(),
+            rabbitmq_host: String.t(),
+            rabbitmq_exchange: String.t(),
+            rabbitmq_topics: list(String.t())
+          }
+
+    defstruct [
+      :repo,
+      :rabbitmq_user,
+      :rabbitmq_password,
+      :rabbitmq_host,
+      :rabbitmq_exchange,
+      :rabbitmq_topics
+    ]
   end
 end
