0d79f0126b9436772a91988aba72d45790e41bf2
Mauricio Giacomini Girardello <mauriciogiacomini4@gmail.com>
2019-09-20 10:10:35 -0300

    Use MessageBroker#get_config/1 for Publisher

--- lib/message_broker/publisher.ex
+++ lib/message_broker/publisher.ex
@@ -1,6 +1,22 @@
 defmodule MessageBroker.Publisher do
   @moduledoc """
-  Publisher for customer_management queues.
+  RabbitMQ Publisher.
+
+  This is a GenServer that keeps a connection open with the broker and know
+  how publish messages to it.
+
+  ## Examples
+
+      iex> {:ok, _pid} = Publisher.start_link
+      {:ok, #PID<0.105.0>}
+
+      iex> Publisher.publish_event(%Event{event_name: "lead.created", payload: %{name: "mauricio"}})
+      {:ok, :ok}
+
+  ## Topics/Queues
+
+  The *event_name* is used as topic/queue name to publish inside exchange.
+
   """
 
   use AMQP
@@ -15,7 +31,7 @@ defmodule MessageBroker.Publisher do
     GenServer.start_link(__MODULE__, opts, name: __MODULE__)
   end
 
-  @callback publish_event(charlist(), struct()) :: {:ok, :ok} | {:error, :topic_not_allowed}
+  @callback publish_event(Event.t()) :: {:ok, :ok} | {:error, :topic_not_allowed}
   def publish_event(%Event{event_name: topic, payload: payload}) do
     event = %{
       event: topic,
@@ -30,7 +46,7 @@ defmodule MessageBroker.Publisher do
     allowed_topics = MessageBroker.get_config(:rabbitmq_topics)
 
     if topic in allowed_topics do
-      GenServer.cast(__MODULE__, {:publish, topic, payload})
+      :ok = GenServer.cast(__MODULE__, {:publish, topic, payload})
       {:ok, :ok}
     else
       {:error, :topic_not_allowed}
@@ -62,44 +78,42 @@ defmodule MessageBroker.Publisher do
   end
 
   defp open_connection do
-    config = MessageBroker.config()
-    user = config.rabbitmq_user
-    password = config.rabbitmq_password
-    host = config.rabbitmq_host
+    user = MessageBroker.get_config(:rabbitmq_user)
+    password = MessageBroker.get_config(:rabbitmq_password)
+    host = MessageBroker.get_config(:rabbitmq_host)
 
     Connection.open("amqp://#{user}:#{password}@#{host}")
   end
 
-  @impl true
+  @impl GenServer
   def init(_opts) do
     rabbitmq_connect()
   end
 
-  @impl true
+  @impl GenServer
   def handle_info({:EXIT, _pid, :shutdown}, _state) do
     File.touch("rabbit_error")
     Logger.info("Publisher connection has died (:EXIT), therefore I have to die as well.")
     Process.exit(self(), :kill)
   end
 
-  @impl true
+  @impl GenServer
   def handle_info({:DOWN, _, :process, _pid, reason}, _state) do
     File.touch("rabbit_error")
 
-    Logger.error(
-      "Publisher connection has died (:DOWN), therefore I have to die as well.\nReason: #{
-        inspect(reason)
-      }"
-    )
+    Logger.error("""
+    Publisher connection has died (:DOWN), therefore I have to die as well.\n
+    Reason: #{inspect(reason)}
+    """)
 
     Process.exit(self(), :kill)
   end
 
-  @impl true
+  @impl GenServer
   def handle_cast({:publish, topic, payload}, channel) do
     :ok = Basic.publish(channel, exchange(), topic, payload, persistent: true)
     {:noreply, channel}
   end
 
-  defp exchange, do: MessageBroker.config().rabbitmq_exchange
+  defp exchange, do: MessageBroker.get_config(:rabbitmq_exchange)
 end
