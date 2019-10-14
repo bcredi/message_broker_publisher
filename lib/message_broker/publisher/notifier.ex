c7ed2d094150eb6ad902adae58fe6c0530d57237
Mateus Rambo Strey <mateus.rstrey@gmail.com>
2019-10-14 11:53:30 -0300

    add lock for publisher

--- lib/message_broker/publisher/notifier.ex
+++ lib/message_broker/publisher/notifier.ex
@@ -9,6 +9,8 @@ defmodule MessageBroker.Publisher.Notifier do
 
   use GenServer
 
+  import Ecto.Query
+
   alias Ecto.Multi
   alias Postgrex.Notifications
 
@@ -44,26 +46,12 @@ defmodule MessageBroker.Publisher.Notifier do
         {:notification, _pid, _ref, @channel, payload},
         %{publisher_name: publisher_name, repo: repo} = state
       ) do
-    with {:ok, %{"record" => %{"id" => id}}} <- Jason.decode(payload),
-         event <- get_event!(repo, id) do
-      Logger.info("Processing message broker event: #{inspect(event)}")
+    case Jason.decode(payload) do
+      {:ok, %{"record" => %{"id" => id}}} ->
+        process_event(id, repo, publisher_name, state)
 
-      Multi.new()
-      |> Multi.delete(:delete_event, event)
-      |> Multi.run(:publish_message, fn _, _ ->
-        Publisher.publish_event(publisher_name, event)
-      end)
-      |> repo.transaction()
-      |> case do
-        {:ok, _changes} ->
-          {:noreply, state}
-
-        {:error, action, error, _} ->
-          mark_as_error!(repo, event, action, error)
-          {:noreply, state}
-      end
-    else
-      error -> {:stop, error, []}
+      error ->
+        {:stop, error, []}
     end
   end
 
@@ -71,7 +59,57 @@ defmodule MessageBroker.Publisher.Notifier do
     {:noreply, state}
   end
 
-  defp get_event!(repo, id), do: repo.get!(Event, id)
+  defp process_event(id, repo, publisher_name, state) do
+    Multi.new()
+    |> Multi.run(:event, fn _, _ ->
+      case get_event!(repo, id) do
+        nil -> {:error, :event_not_found}
+        :lock_not_available -> {:error, :event_locked}
+        %Event{} = event -> {:ok, event}
+        error -> {:error, error}
+      end
+    end)
+    |> Multi.delete(:delete_event, fn %{event: event} -> event end)
+    |> Multi.run(:publish_message, fn _, %{event: event} ->
+      Logger.info("Processing message broker event: #{inspect(event)}")
+      Publisher.publish_event(publisher_name, event)
+    end)
+    |> repo.transaction()
+    |> process_event_result(id, repo, state)
+  end
+
+  defp get_event!(repo, id) do
+    Event
+    |> where([e], e.id == ^id)
+    |> lock("FOR UPDATE NOWAIT")
+    |> repo.one()
+  rescue
+    e in Postgrex.Error ->
+      %{postgres: %{code: error_code}} = e
+      error_code
+  end
+
+  def process_event_result(result, id, repo, state) do
+    case result do
+      {:ok, _changes} ->
+        {:noreply, state}
+
+      {:error, :event, :event_not_found, _} ->
+        Logger.debug("Event ##{id} not found.")
+        {:noreply, state}
+
+      {:error, :event, :event_locked, _} ->
+        Logger.debug("Event ##{id} locked.")
+        {:noreply, state}
+
+      {:error, :event, sql_error, _} ->
+        {:stop, "SQL Error: #{sql_error}", []}
+
+      {:error, action, error, %{event: %Event{} = event}} ->
+        mark_as_error!(repo, event, action, error)
+        {:noreply, state}
+    end
+  end
 
   defp mark_as_error!(repo, %{status: status} = event, action, error) do
     {:ok, now} = DateTime.now("Etc/UTC")
