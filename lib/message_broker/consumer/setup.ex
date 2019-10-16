7269b862d6d8425a4ca2b9c1f80a4f1399de2079
Mateus Rambo Strey <mateus.rstrey@gmail.com>
2019-10-16 15:10:38 -0300

    change message_retrier queue creation

--- lib/message_broker/consumer/setup.ex
+++ lib/message_broker/consumer/setup.ex
@@ -9,6 +9,8 @@ defmodule MessageBroker.Consumer.Setup do
 
   require Logger
 
+  alias MessageBroker.Consumer.MessageRetrierHelper
+
   @doc """
   Initialize Consumer in RabbitMQ server.
   """
@@ -19,7 +21,8 @@ defmodule MessageBroker.Consumer.Setup do
           rabbitmq_host: host,
           rabbitmq_exchange: exchange,
           rabbitmq_queue: queue,
-          rabbitmq_subscribed_topics: subscribed_topics
+          rabbitmq_subscribed_topics: subscribed_topics,
+          rabbitmq_retries_count: retries_count
         } = config
       ) do
     Logger.info("Connecting to RabbitMQ to setup topics for queue.")
@@ -27,7 +30,7 @@ defmodule MessageBroker.Consumer.Setup do
     case rabbitmq_connection(user, password, host) do
       {:ok, conn} ->
         {:ok, chan} = Channel.open(conn)
-        setup_queue(chan, exchange, queue, subscribed_topics)
+        setup_queue(chan, exchange, queue, subscribed_topics, retries_count)
 
       {:error, error} ->
         Logger.error(
@@ -43,7 +46,7 @@ defmodule MessageBroker.Consumer.Setup do
     Connection.open(username: user, password: password, host: host, virtual_host: "/")
   end
 
-  defp setup_queue(chan, exchange, queue, subscribed_topics) do
+  defp setup_queue(chan, exchange, queue, subscribed_topics, retries_count) do
     queue_error = "#{queue}_error"
 
     {:ok, _} = Queue.declare(chan, queue_error, durable: true)
@@ -63,5 +66,34 @@ defmodule MessageBroker.Consumer.Setup do
     for topic <- subscribed_topics do
       :ok = Queue.bind(chan, queue, exchange, routing_key: topic)
     end
+
+    create_retry_queues(retries_count, chan, exchange, queue)
+  end
+
+  defp create_retry_queues(retries_count, channel, exchange, queue)
+
+  defp create_retry_queues(retries_count, _channel, _exchange, _queue) when retries_count < 1,
+    do: :ok
+
+  defp create_retry_queues(retries_count, channel, exchange, queue) when retries_count > 0 do
+    Enum.each(1..retries_count, fn count ->
+      delay = MessageRetrierHelper.exponential_delay_milliseconds(count - 1)
+      create_retry_queue(channel, exchange, queue, delay)
+    end)
+  end
+
+  defp create_retry_queue(channel, exchange, queue, delay) do
+    {:ok, %{queue: retry_queue}} =
+      Queue.declare(channel, "#{queue}.retry.#{delay}",
+        durable: true,
+        arguments: [
+          {"x-dead-letter-exchange", :longstr, ""},
+          {"x-dead-letter-routing-key", :longstr, queue},
+          {"x-message-ttl", :long, delay}
+        ]
+      )
+
+    routing_key = MessageRetrierHelper.routing_key(queue, delay)
+    :ok = Queue.bind(channel, retry_queue, exchange, routing_key: routing_key)
   end
 end
